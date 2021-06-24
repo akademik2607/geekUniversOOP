b19c8d496589e5dc0c70c9b979508a6eae0d9054
akademik2607 <akademik2607@mail.ru>
2021-06-24 18:11:58 +0300

    Выполнил задание к 2 уроку

--- main.cpp
+++ main.cpp
@@ -1,136 +1,148 @@
 #include<iostream>
-#include<cmath>
-#include<cstdint>
-#include<climits>
+#include<vector>
 
+//1.Создать класс Person (человек) с полями: имя, возраст, пол и вес. 
+//Определить методы переназначения имени, изменения возраста и веса. 
+//Создать производный класс Student (студент), имеющий поле года обучения. 
+//Определить методы переназначения и увеличения этого значения. 
+//Создать счетчик количества созданных студентов. В функции main() создать несколько студентов. 
+//По запросу вывести определенного человека.
 
-//1. Создать класс Power, который содержит два вещественных числа. 
-//Этот класс должен иметь две переменные-члена для хранения этих вещественных чисел. 
-//Еще создать два метода: один с именем set, который позволит присваивать значения переменным, второй — calculate,
-//который будет выводить результат возведения первого числа в степень второго числа. Задать значения этих двух чисел по умолчанию.
-
-
-class Power{
+class Person{
     private:
-        double _value;
-        double _pow;
+        std::string m_name;
+        int m_age;
+        char m_sex;
+        double m_weight;
     public:
-        Power( double value=2, double pow=3) 
-            : _value(value), _pow(pow) {}
+        Person(std::string name, int age, char sex, double weight)
+            : m_name(name), m_age(age), m_sex(sex), m_weight(weight) {}
 
-        void set(double value, double pow) {
-            _value = value;
-            _pow = pow;
+        void setName(std::string name){
+            m_name = name;
         }
-
-        void calculate() {
-            std::cout <<_value << " ^ " << _pow << " = " << pow(_value, _pow) << std::endl;
+        void setAge(int age){
+            m_age = age;
+        }
+        void setWeight(double weight){
+            m_weight = weight;
+        }
+        void printPersonInfo(){
+            std::cout << "Имя: " << m_name << std::endl;
+            std::cout << "Пол: " << m_sex << std::endl;
+            std::cout << "Возраст: " << m_age << std::endl;
+            std::cout << "Вес: " << m_weight << std::endl;
         }
 };
 
-//2.Написать класс с именем RGBA, который содержит 4 переменные-члена типа std::uint8_t: m_red, m_green, m_blue и m_alpha 
-//(#include cstdint для доступа к этому типу). 
-//Задать 0 в качестве значения по умолчанию для m_red, m_green, m_blue и 255 для m_alpha. 
-//Создать конструктор со списком инициализации членов, 
-//который позволит пользователю передавать значения для m_red, m_blue, m_green и m_alpha. 
-//Написать функцию print(), которая будет выводить значения переменных-членов.
-
-class RGBA{
+class Student : public Person{
     private:
-        std::uint8_t m_red;
-        std::uint8_t m_green;
-        std::uint8_t m_blue;
-        std::uint8_t m_alpha;
+        int m_course;
     public:
-        RGBA(std::uint8_t red=0, std::uint8_t green=0, std::uint8_t blue=0, std::uint8_t alpha = 255) 
-            : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha){}
-        
-        void print(){
-            std::cout << "Цветовая схема: " << "R-" << static_cast<int>(m_red) << ", G-" << static_cast<int>(m_green) << ", B-" 
-                << static_cast<int>(m_blue) << ", A-" << static_cast<int>(m_alpha) << std::endl;
+        Student(std::string name, int age, char sex, double weight, int course =1 ) 
+            : Person(name, age, sex, weight), m_course(course){
+                counter++;
+                students.push_back(this);
+            }
+        void yearEnd(){
+            if(m_course < 5){
+                m_course++;
+            }
+        }
+        void AssignCourse(int course){
+            if(course < 0 || course > 5){
+                std::cout << "Некорректное назначение!" << std::endl;
+                return;
+            }
+            m_course = course;
+        }
+        void printStudentInfo(){
+            printPersonInfo();
+            std::cout << "Курс: " << m_course<< std::endl;
+        }
+    static int counter;
+    static std::vector<Student*> students;
+    ~Student(){
+        counter--;
+    }
+    static void searchStudent(int num);
+};
+
+    void Student::searchStudent(int num){
+        if(num - 1 > students.size() || num - 1 <= 0){
+            std::cout << "Студент с таким номером не числится!" << std::endl;
+        }
+        else {
+            std::cout << "Даные студента под номером " << num << " - " << std::endl;
+            students[num - 1]->printStudentInfo();
+        }
+    }
+
+int Student::counter = 0;
+std::vector<Student*> Student::students;
+
+//2.Создать классы Apple (яблоко) и Banana (банан), которые наследуют класс Fruit (фрукт).
+// У Fruit есть две переменные-члена: name (имя) и color (цвет). 
+// Добавить новый класс GrannySmith, который наследует класс Apple.
+
+class Fruit{
+    private:
+        std::string m_name;
+        std::string m_color;
+    public:
+        Fruit(std::string color, std::string name="Fruit") 
+            : m_color(color), m_name(name){}
+        std::string getName(){
+            return m_name;
+        }
+        std::string getColor(){
+            return m_color;
+        }
+        void setName(std::string name){
+            m_name = name;
+        }
+        void setColor(std::string color){
+            m_color = color;
         }
 };
 
-//3.Написать класс, который реализует функциональность стека. Класс Stack должен иметь:
-//private-массив целых чисел длиной 10;
-//private целочисленное значение для отслеживания длины стека;
-//public-метод с именем reset(), который будет сбрасывать длину и все значения элементов на 0;
-//public-метод с именем push(), который будет добавлять значение в стек. push() должен возвращать значение false, если массив уже заполнен, и true в противном случае;
-//public-метод с именем pop() для вытягивания и возврата значения из стека. Если в стеке нет значений, то должно выводиться предупреждение;
-//public-метод с именем print(), который будет выводить все значения стека.
-
-class Stack{
-    private:
-        int arr[10];
-        int length;
+class Apple : public Fruit{
     public:
-        Stack() : length(0){}
-
-        void reset(){
-            length = 0;
-            for(int i = 0; i < 10; i++){
-                arr[i] = 0;
-            }
-        }
-
-        bool push(int val){
-            if(length == 10) return false;
-
-            arr[length] = val;
-            length++;
-            return true;
-        }
-
-        int pop(){
-            if(length == 0){
-                std::cout << "Стек пуст!" << std::endl;
-                return INT_MAX;
-            }
-            length--;
-            return arr[length];
-        }
-
-        void print(){
-            std::cout << "( ";
-            for(int i = 0; i < length;i++){
-                std::cout << arr[i] << " ";
-            }
-            std::cout << ")" << std::endl;
-        }
-
+        Apple(std::string color="green", std::string name="Apple") : Fruit(color, name){}
 
 };
 
+class Banana : public Fruit{
+    public:
+        Banana(std::string color="yellow", std::string name="Banana") : Fruit(color, name){}
 
-int main(){
-    std::cout << "1 задание: " << std::endl;
-    Power p(3);
-    p.calculate();
-    p.set(4, 3);
-    p.calculate();
-    std::cout << std::endl;
+};
 
-    std::cout << "2 задание: " << std::endl;
-    RGBA rgba(23, 45, 55);
-    rgba.print();
-    std::cout << std::endl;
+class GrannySmith : public Apple{
+    public:
+        GrannySmith(std::string color="green", std::string name="Granny Smith") : Apple(color, name){}
+};
 
-    std::cout << "3 задание: " << std::endl;
-    Stack stack;
-    stack.reset();
-    stack.print();
+//3. Базовый класс - Player производные: Diller, User
+//класс Stek содержит Card
+//enum value
+//класс game
 
-    stack.push(3);
-    stack.push(7);
-    stack.push(5);
-    stack.print();
+int main() {
+    //1.
+    Student s1("Alex", 21, 'm', 61);
+    Student s2("Nadejda", 20, 'w', 45);
+    Student s3("Anton", 30, 'w', 40);
+    std::cout << Student::counter << std::endl;
+    Student::searchStudent(2);
+    //2.
+    Apple a("red");
+    Banana b;
+    GrannySmith c;
 
-    stack.pop();
-    stack.print();
-
-    stack.pop();
-    stack.pop();
-    stack.print();
-
+    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
     return 0;
 }
+
